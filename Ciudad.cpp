42970a8e2d0ca2561d18d7f2ed54a2686e675e95
OnAFriday <56298731+OnAFriday@users.noreply.github.com>
2019-10-28 12:39:34 -0300

    recorridoDirectoTren(), estanEnElMismoRamal()
    Hay modularizar el primer metodo y agregar opciones de ingreso de datos de coordenadas origen y destino para los recorridos

--- Ciudad.cpp
+++ Ciudad.cpp
@@ -51,6 +51,71 @@ Ciudad::Ciudad(){
 	}
 
 }
+/*Hay que modularizar todo este metodo*/
+Lista<Tren*> Ciudad::recorridoDirectoTren(){
+	/*cambiar el pedido de coordenadas de origen y destino*/
+	Coordenadas origen=this->pedirOrigen();
+	Coordenadas destino=this->pedirDestino();
+	Lista<Tren*> recorrido;
+	Lista<Tren*> estacionesOrigen;
+	Lista<Tren*> estacionesDestino;
+	/*busca las estaciones por condicion de distancia*/
+	this->estacionesTren->iniciarCursor();
+	while(this->estacionesTren->avanzarCursor()){
+		Tren* estacionIterada=estacionesTren->obtenerCursor();
+		Coordenadas * ubicacionEstacion=estacionIterada->verUbicacion();
+		if(ubicacionEstacion->distanciaMetros(origen) < 500) {
+			estacionesOrigen.agregar(estacionIterada);
+		}
+		else if(ubicacionEstacion->distanciaMetros(destino) < 500){
+			estacionesDestino.agregar(estacionIterada);
+		}
+	}
+	/*verifica si hay coincidecia de ramal para todas las estaciones de tren de origen y destino. Por ej
+	lee un registro de origen e itera completamente por los de destino. Si encuentra coinidencia
+	de ramal termina el recorrido y sigue abajo imprimiendo. Si no encuentra, avanza en la
+	lista origen y hace el mismo procedimiento*/
+	estacionesOrigen.iniciarCursor();
+	estacionesDestino.iniciarCursor();
+	bool recorridoEncontrado =false;
+	while(estacionesOrigen.avanzarCursor()&&!recorridoEncontrado){
+		Tren* estacionIteradaOrigen=estacionesOrigen.obtenerCursor();
+		while(estacionesDestino.avanzarCursor()){
+			Tren* estacionIteradaDestino=estacionesDestino.obtenerCursor();
+			if(estanEnElMismoRamal(estacionIteradaOrigen, estacionIteradaDestino)){
+				recorridoEncontrado=true;
+				recorrido.agregar(estacionIteradaOrigen);
+				recorrido.agregar(estacionIteradaDestino);
+			}
+		}
+
+	}
+/*imprime la lista recorrido, hay que llevarlo a otro metodo*/
+	recorrido.iniciarCursor();
+		while(recorrido.avanzarCursor()){
+			Tren* estacion=recorrido.obtenerCursor();
+			std::cout<<estacion->verRamal()<<estacion->verUbicacionLatitud()
+					<<estacion->verUbicacionLongitud()<<
+					estacion->verNombre()<<std::endl;
+		}
+	return recorrido;
+
+}
+
+bool Ciudad::estanEnElMismoRamal(Tren* origen, Tren* destino){
+
+	return origen->verRamal()==destino->verRamal();
+}
+/*arreglar estos metodos con una entrada de datos para hacer pruebas de recorridos*/
+Coordenadas Ciudad::pedirOrigen(){
+	/*longitud, latitud*/
+	Coordenadas origenUsuario("-58.4249824", "-34.5717485");
+	return origenUsuario;
+}
+Coordenadas Ciudad::pedirDestino(){
+	Coordenadas origenUsuario("-58.4618081", "-34.5680506");
+		return origenUsuario;
+}
 
 Ciudad::~Ciudad(){
 
