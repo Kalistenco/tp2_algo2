edc825db933358ae272356010d68c9f96152ddff
Kalistenco <martinkalistnco@gmail.com>
2019-12-06 18:20:52 -0300

    .

--- main.cpp
+++ main.cpp
@@ -11,24 +11,21 @@
 using namespace std;
 int main(){
 
-	/*Ciudad caba;
-	Coordenadas partida("-58.4249824", "-34.5717485");
-	Coordenadas llegada("-58.4618081", "-34.5680506");
-	caba.verRecorridoDirecto(partida, llegada);
-	caba.verRecorridoConCombinacion(partida, llegada);*/
+	// Ciudad caba;
+	// Coordenadas partida("-58.4249824", "-34.5717485");
+	// Coordenadas llegada("-58.4618081", "-34.5680506");
+	// caba.verRecorridoDirecto(partida, llegada);
+	// caba.verRecorridoConCombinacion(partida, llegada);
 
-<<<<<<< HEAD
-	Lista<Estacion*>*trenes=caba.verEstacionesTren();
-	cout.precision(10);
-	trenes->iniciarCursor();
-	while(trenes->avanzarCursor()){
-		Estacion * trenIterado=trenes->obtenerCursor();
-		std::cout<<trenIterado->verNombre()<<std::endl;
-	}
-=======
+	// Lista<Estacion*>*trenes=caba.verEstacionesTren();
+	// cout.precision(10);
+	// trenes->iniciarCursor();
+	// while(trenes->avanzarCursor()){
+	// 	Estacion * trenIterado=trenes->obtenerCursor();
+	// 	std::cout<<trenIterado->verNombre()<<std::endl;
+	// }
 	Menu miMenu;
 	miMenu.iniciarMenu();
->>>>>>> 9ea4403ea98fe49371d37a5e2a8b87e69bcf1dd3
 
 /*	Lista<Estacion*>*colectivos=caba.verColectivos();
 	cout.precision(10);
